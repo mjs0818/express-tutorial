a60f5612e82935117d1b205b2df728f1cbd53c88
mjs0818 <tjfaudwo09@gmail.com>
2020-11-01 23:05:20 +0900

    serving static file

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const birds = require('./BasicRouting/birds');
 const routing = require('./BasicRouting/routing');
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.use('/', routing);
 app.use('/birds', birds);
-
-
-
+app.use('/static', express.static(__dirname + '/ServingStatic'));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "ServingStatic", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
